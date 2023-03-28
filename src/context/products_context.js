12e7c8604a3835432e822420749ad2eb7c503f01
Marc Granier <mgranier@comcast.net>
2023-03-28 18:03:40 -0400

    Fetch Products Complete

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -15,6 +15,10 @@ import {
 
 const initialState = {
 	isSidebarOpen: false,
+	products_loading: false,
+	products_error: false,
+	products: [],
+	featured_products: [],
 }
 
 const ProductsContext = React.createContext()
@@ -31,8 +35,14 @@ export const ProductsProvider = ({ children }) => {
 	}
 
 	const fetchProducts = async (url) => {
-		const response = await axios.get(url)
-		console.log(response)
+		dispatch({ type: GET_PRODUCTS_BEGIN })
+		try {
+			const response = await axios.get(url)
+			const products = response.data
+			dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products })
+		} catch (error) {
+			dispatch({ type: GET_PRODUCTS_ERROR })
+		}
 	}
 
 	useEffect(() => {
