2aa4d3aaf2c0fe729a7a4567c66746a4b10d5d3d
Marc Granier <mgranier@comcast.net>
2023-03-28 15:43:28 -0400

    Products Context Setup

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -3,28 +3,40 @@ import React, { useContext, useEffect, useReducer } from 'react'
 import reducer from '../reducers/products_reducer'
 import { products_url as url } from '../utils/constants'
 import {
-  SIDEBAR_OPEN,
-  SIDEBAR_CLOSE,
-  GET_PRODUCTS_BEGIN,
-  GET_PRODUCTS_SUCCESS,
-  GET_PRODUCTS_ERROR,
-  GET_SINGLE_PRODUCT_BEGIN,
-  GET_SINGLE_PRODUCT_SUCCESS,
-  GET_SINGLE_PRODUCT_ERROR,
+	SIDEBAR_OPEN,
+	SIDEBAR_CLOSE,
+	GET_PRODUCTS_BEGIN,
+	GET_PRODUCTS_SUCCESS,
+	GET_PRODUCTS_ERROR,
+	GET_SINGLE_PRODUCT_BEGIN,
+	GET_SINGLE_PRODUCT_SUCCESS,
+	GET_SINGLE_PRODUCT_ERROR,
 } from '../actions'
 
-const initialState = {}
+const initialState = {
+	isSidebarOpen: false,
+}
 
 const ProductsContext = React.createContext()
 
 export const ProductsProvider = ({ children }) => {
-  return (
-    <ProductsContext.Provider value='products context'>
-      {children}
-    </ProductsContext.Provider>
-  )
+	const [state, dispatch] = useReducer(reducer, initialState)
+
+	const openSidebar = () => {
+		dispatch({ type: SIDEBAR_OPEN })
+	}
+
+	const closeSidebar = () => {
+		dispatch({ type: SIDEBAR_CLOSE })
+	}
+
+	return (
+		<ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
+			{children}
+		</ProductsContext.Provider>
+	)
 }
 // make sure use
 export const useProductsContext = () => {
-  return useContext(ProductsContext)
+	return useContext(ProductsContext)
 }
