9aeda114b73e898c8d1f35b8cb5cacf94d83b5f5
Marc Granier <mgranier@comcast.net>
2023-03-30 15:08:14 -0400

    Filter Context Setup

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -1,29 +1,39 @@
 import React, { useEffect, useContext, useReducer } from 'react'
 import reducer from '../reducers/filter_reducer'
 import {
-  LOAD_PRODUCTS,
-  SET_GRIDVIEW,
-  SET_LISTVIEW,
-  UPDATE_SORT,
-  SORT_PRODUCTS,
-  UPDATE_FILTERS,
-  FILTER_PRODUCTS,
-  CLEAR_FILTERS,
+	LOAD_PRODUCTS,
+	SET_GRIDVIEW,
+	SET_LISTVIEW,
+	UPDATE_SORT,
+	SORT_PRODUCTS,
+	UPDATE_FILTERS,
+	FILTER_PRODUCTS,
+	CLEAR_FILTERS,
 } from '../actions'
 import { useProductsContext } from './products_context'
 
-const initialState = {}
+const initialState = {
+	filtered_products: [],
+	all_products: [],
+}
 
 const FilterContext = React.createContext()
 
 export const FilterProvider = ({ children }) => {
-  return (
-    <FilterContext.Provider value='filter context'>
-      {children}
-    </FilterContext.Provider>
-  )
+	const { products } = useProductsContext()
+	const [state, dispatch] = useReducer(reducer, initialState)
+
+	useEffect(() => {
+		dispatch({ type: LOAD_PRODUCTS, payload: products })
+	}, [products])
+
+	return (
+		<FilterContext.Provider value='filter context'>
+			{children}
+		</FilterContext.Provider>
+	)
 }
 // make sure use
 export const useFilterContext = () => {
-  return useContext(FilterContext)
+	return useContext(FilterContext)
 }
