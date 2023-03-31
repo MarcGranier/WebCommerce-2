fcd06c7dc8d4f83e46408cc35f15801c77e4bcd9
Marc Granier <mgranier@comcast.net>
2023-03-31 11:27:13 -0400

    Sort component - View Buttons

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -3,18 +3,41 @@ import { useFilterContext } from '../context/filter_context'
 import { BsFillGridFill, BsList } from 'react-icons/bs'
 import styled from 'styled-components'
 const Sort = () => {
-	const { filtered_products: products, grid_view } = useFilterContext()
+	const {
+		filtered_products: products,
+		grid_view,
+		setListView,
+		setGridView,
+	} = useFilterContext()
 	return (
 		<Wrapper>
 			<div className='btn-container'>
-				<button type='button' className={`${grid_view ? 'active' : null}`}>
+				<button
+					type='button'
+					className={`${grid_view ? 'active' : null}`}
+					onClick={setGridView}
+				>
 					<BsFillGridFill />
 				</button>
-				<button type='button' className={`${!grid_view ? 'active' : null}`}>
+				<button
+					type='button'
+					className={`${!grid_view ? 'active' : null}`}
+					onClick={setListView}
+				>
 					<BsList />
 				</button>
 			</div>
 			<p>{products.length} products found</p>
+			<hr />
+			<form>
+				<label htmlFor='sort'>sort by</label>
+				<select name='sort' id='sort' className='sort-input'>
+					<option value='price-lowest'>price (lowest)</option>
+					<option value='price-highest'>price (highest)</option>
+					<option value='name-a'>name (a-z)</option>
+					<option value='name-z'>name (z-a)</option>
+				</select>
+			</form>
 		</Wrapper>
 	)
 }
