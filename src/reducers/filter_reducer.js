950995de8da610d07b6faaa6fc5a0581a8fa5a86
Marc Granier <mgranier@comcast.net>
2023-03-31 16:31:08 -0400

    Filters - Text

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -49,7 +49,16 @@ const filter_reducer = (state, action) => {
 				return b.name.localeCompare(a.name)
 			})
 		}
+
 		return { ...state, filtered_products: tempProducts }
+	}
+	if (action.type === UPDATE_FILTERS) {
+		const { name, value } = action.payload
+		return { ...state, filters: { ...state.filters, [name]: value } }
+	}
+	if (action.type === FILTER_PRODUCTS) {
+		console.log('filtering products')
+		return { ...state }
 	}
 
 	throw new Error(`No Matching "${action.type}" - action type`)
