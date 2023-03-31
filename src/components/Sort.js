0f3cc86689dbf7cf15c0b0e6621463dc98a6005e
Marc Granier <mgranier@comcast.net>
2023-03-31 12:43:57 -0400

    Sort Component - Controlled input & Sort functionality

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -8,6 +8,8 @@ const Sort = () => {
 		grid_view,
 		setListView,
 		setGridView,
+		sort,
+		updateSort,
 	} = useFilterContext()
 	return (
 		<Wrapper>
@@ -31,7 +33,13 @@ const Sort = () => {
 			<hr />
 			<form>
 				<label htmlFor='sort'>sort by</label>
-				<select name='sort' id='sort' className='sort-input'>
+				<select
+					name='sort'
+					id='sort'
+					className='sort-input'
+					value={sort}
+					onChange={updateSort}
+				>
 					<option value='price-lowest'>price (lowest)</option>
 					<option value='price-highest'>price (highest)</option>
 					<option value='name-a'>name (a-z)</option>
