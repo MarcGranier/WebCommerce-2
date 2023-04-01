bc414a5fa33c164afa9759a8f05bfcfc886f21a9
Marc Granier <mgranier@comcast.net>
2023-04-01 14:35:57 -0400

    Filters - Unique Values

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -57,7 +57,7 @@ const filter_reducer = (state, action) => {
 		return { ...state, filters: { ...state.filters, [name]: value } }
 	}
 	if (action.type === FILTER_PRODUCTS) {
-		console.log('filtering products')
+		// console.log('filtering products')
 		return { ...state }
 	}
 
