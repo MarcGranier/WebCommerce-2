76a8e8923a589ecd5d94574d164db4673666dc4b
Marc Granier <mgranier@comcast.net>
2023-04-01 15:07:42 -0400

    Filters - Category

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -25,8 +25,6 @@ const Filters = () => {
 	const companies = getUniqueValues(all_products, 'company')
 	const colors = getUniqueValues(all_products, 'colors')
 
-	console.log(colors)
-
 	return (
 		<Wrapper>
 			<div className='content'>
@@ -43,6 +41,28 @@ const Filters = () => {
 						/>
 					</div>
 					{/* end of search input */}
+					{/* categories */}
+					<div className='form-control'>
+						<h5>category</h5>
+						<div>
+							{categories.map((c, index) => {
+								return (
+									<button
+										key={index}
+										onClick={updateFilters}
+										type='button'
+										name='category'
+										className={`${
+											category === c.toLowerCase() ? 'active' : null
+										}`}
+									>
+										{c}
+									</button>
+								)
+							})}
+						</div>
+					</div>
+					{/* end of categories */}
 				</form>
 			</div>
 		</Wrapper>
