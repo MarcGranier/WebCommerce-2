f562988d0af4e9e0a65477aabd14468d680f6e26
Marc Granier <mgranier@comcast.net>
2023-04-01 17:14:48 -0400

    Filters  Color

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -82,6 +82,44 @@ const Filters = () => {
 						</select>
 					</div>
 					{/* end of companies */}
+					{/* colors */}
+					<div className='form-control'>
+						<h5>colors</h5>
+						<div className='colors'>
+							{colors.map((c, index) => {
+								if (c === 'all') {
+									return (
+										<button
+											key={index}
+											name='color'
+											className={`${
+												color === 'all' ? 'all-btn active' : 'all-btn'
+											}`}
+											data-color='all'
+											onClick={updateFilters}
+										>
+											all
+										</button>
+									)
+								}
+								return (
+									<button
+										key={index}
+										name='color'
+										style={{ background: c }}
+										className={`${
+											color === c ? 'color-btn active' : 'color-btn'
+										}`}
+										data-color={c}
+										onClick={updateFilters}
+									>
+										{color === c ? <FaCheck /> : null}
+									</button>
+								)
+							})}
+						</div>
+					</div>
+					{/* end of colors */}
 				</form>
 			</div>
 		</Wrapper>
