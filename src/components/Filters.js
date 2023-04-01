75aeae24e632a333c90a9abba36924d7a4017448
Marc Granier <mgranier@comcast.net>
2023-04-01 15:24:09 -0400

    Filters - Company

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -63,6 +63,25 @@ const Filters = () => {
 						</div>
 					</div>
 					{/* end of categories */}
+					{/* companies */}
+					<div className='form-control'>
+						<h5>company</h5>
+						<select
+							name='company'
+							value={company}
+							onChange={updateFilters}
+							className='company'
+						>
+							{companies.map((c, index) => {
+								return (
+									<option key={index} value={c}>
+										{c}
+									</option>
+								)
+							})}
+						</select>
+					</div>
+					{/* end of companies */}
 				</form>
 			</div>
 		</Wrapper>
