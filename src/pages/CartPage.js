636e4a49af40403d7b1401265a3ff7f1c3373f53
Marc Granier <mgranier@comcast.net>
2023-04-02 11:47:36 -0400

    Cart Page Setup

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -5,17 +5,31 @@ import { Link } from 'react-router-dom'
 import { CartContent, PageHero } from '../components'
 
 const CartPage = () => {
-  return <h4>cart page</h4>
+	const { cart } = useCartContext()
+	if (cart.length < 1) {
+		return (
+			<Wrapper className='page-100'>
+				<div className='empty'>
+					<h2>Your cart is empty</h2>
+					<Link to='/products' className='btn'>
+						fill it
+					</Link>
+				</div>
+			</Wrapper>
+		)
+	}
+
+	return <main></main>
 }
 
 const Wrapper = styled.main`
-  .empty {
-    text-align: center;
-    h2 {
-      margin-bottom: 1rem;
-      text-transform: none;
-    }
-  }
+	.empty {
+		text-align: center;
+		h2 {
+			margin-bottom: 1rem;
+			text-transform: none;
+		}
+	}
 `
 
 export default CartPage
