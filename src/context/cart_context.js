5468a2ea946e0b4788548ab34e6727c5721d82ee
Marc Granier <mgranier@comcast.net>
2023-04-02 10:13:09 -0400

    AddtoCart Reducer - Existing Item

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -9,7 +9,7 @@ import {
 } from '../actions'
 
 const initialState = {
-	cart: {},
+	cart: [],
 	total_items: 0,
 	total_amount: 0,
 	shipping_fee: 534,
@@ -20,13 +20,14 @@ const CartContext = React.createContext()
 export const CartProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(reducer, initialState)
 
-	//addToCart
 	const addToCart = (id, color, amount, product) => {
 		dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } })
 	}
 
 	return (
-		<CartContext.Provider value={{ ...state }}>{children}</CartContext.Provider>
+		<CartContext.Provider value={{ ...state, addToCart }}>
+			{children}
+		</CartContext.Provider>
 	)
 }
 // make sure use
