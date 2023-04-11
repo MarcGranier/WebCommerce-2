78c14be0d60773dd044ddef17eec3e903ffd6804
Marc Granier <mgranier@comcast.net>
2023-04-11 11:58:00 -0400

    Auth0 - Toggle Values

--- src/context/user_context.js
+++ src/context/user_context.js
@@ -8,9 +8,11 @@ export const UserProvider = ({ children }) => {
 	const [myUser, setMyUser] = useState(null)
 
 	useEffect(() => {
-		console.log(`user':${user}`)
-		console.log(`isAuthenticated':${isAuthenticated}`)
-		console.log(`isLoading': ${isLoading}`)
+		if (isAuthenticated) {
+			setMyUser(user)
+		} else {
+			setMyUser(false)
+		}
 	}, [isAuthenticated])
 
 	return (
