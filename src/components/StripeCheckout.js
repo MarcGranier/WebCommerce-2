f5417929c5353abcf03fb151ba44a8ee43e7d891
Marc Granier <mgranier@comcast.net>
2023-04-12 17:02:11 -0400

    STRIPE checkout - Basic Return

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -20,7 +20,7 @@ const CheckoutForm = () => {
 	const { myUser } = useUserContext()
 	const history = useHistory()
 	//STRIPE STUFF
-	const [succeeded, setSucceeded] = useState(false)
+	const [succeeded, setSucceeded] = useState(true)
 	const [error, setError] = useState(null)
 	const [processing, setProcessing] = useState('')
 	const [disabled, setDisabled] = useState(true)
@@ -45,7 +45,53 @@ const CheckoutForm = () => {
 			}
 		}
 	}
-	return <h4>hello from Stripe Checkout </h4>
+
+	const createPaymentIntent = async () => {
+		console.log('hello from stripe checkout')
+	}
+
+	useEffect(() => {
+		createPaymentIntent()
+		// eslint-disable-next-line
+	}, [])
+
+	const handleChange = async (event) => {}
+	const handleSubmit = async (ev) => {}
+
+	return (
+		<div>
+			<form id='payment-form' onSubmit={handleSubmit}>
+				<CardElement
+					id='card-element'
+					options={cardStyle}
+					onChange={handleChange}
+				/>
+				<button disabled={processing || disabled || succeeded} id='submit'>
+					<span id='button text'>
+						{processing ? (
+							<div className='spinner' id='spinner'></div>
+						) : (
+							'Pay now'
+						)}
+					</span>
+				</button>
+				{/* Show any error that happens when processing the payment */}
+				{error && (
+					<div className='card-error' role='alert'>
+						{error}
+					</div>
+				)}
+				{/* Show a success message upon completion */}
+				<p className={succeeded ? 'result-message' : 'result-message hidden'}>
+					Payment succeeded, see the result in your{' '}
+					<a href={`https://dashboard.stripe.com/test/payments`}>
+						Stripe dashboard.{'  '}
+					</a>
+					Refresh the page to pay again.
+				</p>
+			</form>
+		</div>
+	)
 }
 
 const StripeCheckout = () => {
